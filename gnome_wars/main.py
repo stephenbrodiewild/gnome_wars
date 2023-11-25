7862a6dffa88e1af032b860b61587a6ceeeb6869
sbw <stephenbrodiewild@gmail.com>
2023-11-25 19:58:22 +0800

    Basic game loop

--- gnome_wars/main.py
+++ gnome_wars/main.py
@@ -1,6 +1,31 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+
 def main():
-    print("Hello, world!")
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
 
+    while running:
+        # poll for events
+        for event in pygame.event.get():
+            # Check for keydown event
+            if event.type == pygame.KEYDOWN:
+                # Check if the 'Q' key is pressed
+                if event.key == pygame.K_q:
+                    running = False
 
-if __name__ == "__main__":
-    main()
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+
+        # RENDER YOUR GAME HERE
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
