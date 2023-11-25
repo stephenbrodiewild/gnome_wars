ae9559ba2f029ae981e7b78032cf8cc7d38ca151
sbw <stephenbrodiewild@gmail.com>
2023-11-25 20:38:57 +0800

    Logging config

---
+++ logging_config.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def setup_logging():
+    # Create a basic configuration for logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    # Add more configurations or handlers if needed
