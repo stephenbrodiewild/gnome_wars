a631dfb581d9f1abf235f49dc8dd22c1c5cb688e
sbw <stephenbrodiewild@gmail.com>
2023-12-02 12:45:00 +0800

    messy asset fuck around

---
+++ assets/tilemaps/test tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="test tileset" tilewidth="56" tileheight="56" tilecount="324" columns="18">
+ <image source="../tileset.png" width="1024" height="1024"/>
+</tileset>
